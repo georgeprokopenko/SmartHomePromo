3ccef8a7e6373f18584b0158424ff830dcaeb39c
George Prokopenko <georgeprokopenko@georges-imac.local>
2018-07-02 22:32:32 +0300

    Initial Commit

---
+++ SmartHome Promo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartHome Promo
+//
+//  Created by George Prokopenko on 02/07/2018.
+//  Copyright © 2018 George Prokopenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartHome Promo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SmartHome Promo
+//
+//  Created by George Prokopenko on 02/07/2018.
+//  Copyright © 2018 George Prokopenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
